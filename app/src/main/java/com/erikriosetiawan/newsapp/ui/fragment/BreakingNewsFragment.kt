35e2bfa13ce4c5a4dedb0eb9027afeb2cb5a9205
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-05-20 18:46:01 +0700

    Restructuring the package

--- app/src/main/java/com/erikriosetiawan/newsapp/ui/fragment/BreakingNewsFragment.kt
+++ app/src/main/java/com/erikriosetiawan/newsapp/ui/fragment/BreakingNewsFragment.kt
@@ -1,6 +1,7 @@
-package com.erikriosetiawan.newsapp
+package com.erikriosetiawan.newsapp.ui.fragment
 
 import androidx.fragment.app.Fragment
+import com.erikriosetiawan.newsapp.R
 
 class BreakingNewsFragment : Fragment(R.layout.fragment_breaking_news) {
 
--- app/src/main/java/com/erikriosetiawan/newsapp/ui/fragment/SearchNewsFragment.kt
+++ app/src/main/java/com/erikriosetiawan/newsapp/ui/fragment/SearchNewsFragment.kt
@@ -1,6 +1,7 @@
-package com.erikriosetiawan.newsapp
+package com.erikriosetiawan.newsapp.ui.fragment
 
 import androidx.fragment.app.Fragment
+import com.erikriosetiawan.newsapp.R
 
 class SearchNewsFragment : Fragment(R.layout.fragment_search_news) {
 
