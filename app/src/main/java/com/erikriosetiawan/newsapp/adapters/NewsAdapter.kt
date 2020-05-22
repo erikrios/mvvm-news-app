9eaabd1529f5dd7d050e301b82d966b823fd52e5
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-05-22 10:46:00 +0700

    Add differCallback

--- app/src/main/java/com/erikriosetiawan/newsapp/adapters/NewsAdapter.kt
+++ app/src/main/java/com/erikriosetiawan/newsapp/adapters/NewsAdapter.kt
@@ -1,11 +1,24 @@
 package com.erikriosetiawan.newsapp.adapters
 
 import android.view.View
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
+import com.erikriosetiawan.newsapp.models.Article
 
 class NewsAdapter : RecyclerView.Adapter<NewsAdapter.ArticleViewHolder>() {
 
     inner class ArticleViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
+    }
+
+    private val differCallback = object : DiffUtil.ItemCallback<Article>() {
+
+        override fun areItemsTheSame(oldItem: Article, newItem: Article): Boolean {
+            return oldItem.url == newItem.url
+        }
+
+        override fun areContentsTheSame(oldItem: Article, newItem: Article): Boolean {
+            oldItem == newItem
+        }
     }
 }
