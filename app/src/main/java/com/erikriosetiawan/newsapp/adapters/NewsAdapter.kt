f01d5e534f123bf254c9128f062cb87435f5ba13
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-05-22 10:39:25 +0700

    Create ArticleViewHolder

--- app/src/main/java/com/erikriosetiawan/newsapp/adapters/NewsAdapter.kt
+++ app/src/main/java/com/erikriosetiawan/newsapp/adapters/NewsAdapter.kt
@@ -1,2 +1,11 @@
 package com.erikriosetiawan.newsapp.adapters
 
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class NewsAdapter : RecyclerView.Adapter<NewsAdapter.ArticleViewHolder>() {
+
+    inner class ArticleViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    }
+}
