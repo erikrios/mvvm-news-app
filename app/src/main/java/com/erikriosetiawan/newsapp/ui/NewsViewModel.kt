c60152f4c8ba67a356323e133109bca64042245b
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-05-26 10:26:45 +0700

    Create NewsViewModel.kt

---
+++ app/src/main/java/com/erikriosetiawan/newsapp/ui/NewsViewModel.kt
@@ -0,0 +1,8 @@
+package com.erikriosetiawan.newsapp.ui
+
+import androidx.lifecycle.ViewModel
+import com.erikriosetiawan.newsapp.repository.NewsRepository
+
+class NewsViewModel(newsRepository: NewsRepository) : ViewModel() {
+
+}
