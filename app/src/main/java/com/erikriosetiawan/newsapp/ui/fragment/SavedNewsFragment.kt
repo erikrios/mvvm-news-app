245a331c46234ad737a1678e94be3b68e0b9fa42
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-05-26 10:38:45 +0700

    Implements the view model on SavedNewsFragment.kt

--- app/src/main/java/com/erikriosetiawan/newsapp/ui/fragment/SavedNewsFragment.kt
+++ app/src/main/java/com/erikriosetiawan/newsapp/ui/fragment/SavedNewsFragment.kt
@@ -1,8 +1,18 @@
 package com.erikriosetiawan.newsapp.ui.fragment
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import com.erikriosetiawan.newsapp.R
+import com.erikriosetiawan.newsapp.ui.NewsActivity
+import com.erikriosetiawan.newsapp.ui.NewsViewModel
 
 class SavedNewsFragment : Fragment(R.layout.fragment_saved_news) {
 
+    lateinit var viewModel: NewsViewModel
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel = (activity as NewsActivity).viewModel
+    }
 }
