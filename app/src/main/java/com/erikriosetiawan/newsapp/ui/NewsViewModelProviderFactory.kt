3b8201ece28324e9dbbdbe870e3bdd5d3ec91466
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-05-26 10:33:35 +0700

    Rename the class

--- app/src/main/java/com/erikriosetiawan/newsapp/ui/NewsViewModelProviderFactory.kt
+++ app/src/main/java/com/erikriosetiawan/newsapp/ui/NewsViewModelProviderFactory.kt
@@ -5,7 +5,7 @@ import androidx.lifecycle.ViewModelProvider
 import com.erikriosetiawan.newsapp.repository.NewsRepository
 
 @Suppress("UNCHECKED_CAST")
-class NewsViewModelFactory(val newsRepository: NewsRepository) : ViewModelProvider.Factory {
+class NewsViewModelProviderFactory(val newsRepository: NewsRepository) : ViewModelProvider.Factory {
 
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
         return NewsViewModel(newsRepository) as T
