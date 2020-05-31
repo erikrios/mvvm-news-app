2e04643e967ed2f81c59ded421687a87d378c3e1
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-05-31 08:44:18 +0700

    Set up the recycler view

--- app/src/main/java/com/erikriosetiawan/newsapp/ui/fragment/BreakingNewsFragment.kt
+++ app/src/main/java/com/erikriosetiawan/newsapp/ui/fragment/BreakingNewsFragment.kt
@@ -3,16 +3,29 @@ package com.erikriosetiawan.newsapp.ui.fragment
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.erikriosetiawan.newsapp.R
+import com.erikriosetiawan.newsapp.adapters.NewsAdapter
 import com.erikriosetiawan.newsapp.ui.NewsActivity
 import com.erikriosetiawan.newsapp.ui.NewsViewModel
+import kotlinx.android.synthetic.main.fragment_breaking_news.*
 
 class BreakingNewsFragment : Fragment(R.layout.fragment_breaking_news) {
 
     lateinit var viewModel: NewsViewModel
+    lateinit var newsAdapter: NewsAdapter
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         viewModel = (activity as NewsActivity).viewModel
+        setUpRecyclerView()
+    }
+
+    private fun setUpRecyclerView() {
+        newsAdapter = NewsAdapter()
+        rvBreakingNews.apply {
+            adapter = newsAdapter
+            layoutManager = LinearLayoutManager(activity)
+        }
     }
 }
