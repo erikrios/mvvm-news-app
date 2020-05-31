5a5da941eaf1835d2ce19b4d929b7a292fc0c482
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-05-31 08:33:20 +0700

    Implement the network handling

--- app/src/main/java/com/erikriosetiawan/newsapp/ui/NewsViewModel.kt
+++ app/src/main/java/com/erikriosetiawan/newsapp/ui/NewsViewModel.kt
@@ -1,8 +1,31 @@
 package com.erikriosetiawan.newsapp.ui
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.erikriosetiawan.newsapp.models.NewsResponse
 import com.erikriosetiawan.newsapp.repository.NewsRepository
+import com.erikriosetiawan.newsapp.util.Resource
+import kotlinx.coroutines.launch
+import retrofit2.Response
 
-class NewsViewModel(newsRepository: NewsRepository) : ViewModel() {
+class NewsViewModel(val newsRepository: NewsRepository) : ViewModel() {
 
+    val breakingNews: MutableLiveData<Resource<NewsResponse>> = MutableLiveData()
+    val breakingNewsPage = 1
+
+    fun getBreakingNews(countryCode: String) = viewModelScope.launch {
+        breakingNews.postValue(Resource.Loading())
+        val response = newsRepository.getBreakingNews(countryCode, breakingNewsPage)
+
+    }
+
+    private fun handleBreakingNewsResponse(response: Response<NewsResponse>): Resource<NewsResponse> {
+        if (response.isSuccessful) {
+            response.body()?.let { resultResponse ->
+                return Resource.Success(resultResponse)
+            }
+        }
+        return Resource.Error(response.message())
+    }
 }
