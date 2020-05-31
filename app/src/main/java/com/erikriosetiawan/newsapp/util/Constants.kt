bbe9a65a0ceabd47a3a94d0e604c088ae7b444c9
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-05-31 09:45:30 +0700

    Add delay for search news

--- app/src/main/java/com/erikriosetiawan/newsapp/util/Constants.kt
+++ app/src/main/java/com/erikriosetiawan/newsapp/util/Constants.kt
@@ -5,5 +5,6 @@ class Constants {
     companion object {
         const val API_KEY = "7601cc8e0d324761ab73016241055955"
         const val BASE_URL = "https://newsapi.org"
+        const val SEARCH_NEWS_TIME_DELAY = 500L
     }
 }
