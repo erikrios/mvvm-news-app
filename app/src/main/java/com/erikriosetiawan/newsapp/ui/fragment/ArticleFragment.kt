c4338863d481e28094414466fb8d1a3f7ae24d1b
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-06-01 13:26:56 +0700

    Add floating action bar click handler to save article into the database

--- app/src/main/java/com/erikriosetiawan/newsapp/ui/fragment/ArticleFragment.kt
+++ app/src/main/java/com/erikriosetiawan/newsapp/ui/fragment/ArticleFragment.kt
@@ -3,11 +3,13 @@ package com.erikriosetiawan.newsapp.ui.fragment
 import android.os.Bundle
 import android.view.View
 import android.webkit.WebViewClient
+import android.widget.SeekBar
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
 import com.erikriosetiawan.newsapp.R
 import com.erikriosetiawan.newsapp.ui.NewsActivity
 import com.erikriosetiawan.newsapp.ui.NewsViewModel
+import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.fragment_article.*
 
 class ArticleFragment : Fragment(R.layout.fragment_article) {
@@ -22,6 +24,11 @@ class ArticleFragment : Fragment(R.layout.fragment_article) {
         webView.apply {
             webViewClient = WebViewClient()
             loadUrl(article.url)
+        }
+
+        fab.setOnClickListener {
+            viewModel.saveArticle(article)
+            Snackbar.make(view, "Article saved successfully", Snackbar.LENGTH_SHORT).show()
         }
     }
 }
