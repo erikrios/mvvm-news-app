218390c53982ca21b878951649530ae8f0c4fbea
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-06-01 12:56:59 +0700

    Add OnItemClickListener handling

--- app/src/main/java/com/erikriosetiawan/newsapp/ui/fragment/SavedNewsFragment.kt
+++ app/src/main/java/com/erikriosetiawan/newsapp/ui/fragment/SavedNewsFragment.kt
@@ -3,16 +3,40 @@ package com.erikriosetiawan.newsapp.ui.fragment
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.erikriosetiawan.newsapp.R
+import com.erikriosetiawan.newsapp.adapters.NewsAdapter
 import com.erikriosetiawan.newsapp.ui.NewsActivity
 import com.erikriosetiawan.newsapp.ui.NewsViewModel
+import kotlinx.android.synthetic.main.fragment_saved_news.*
 
 class SavedNewsFragment : Fragment(R.layout.fragment_saved_news) {
 
     lateinit var viewModel: NewsViewModel
+    lateinit var newsAdapter: NewsAdapter
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         viewModel = (activity as NewsActivity).viewModel
+        setUpRecyclerView()
+
+        newsAdapter.setOnItemClickListener {
+            val bundle = Bundle().apply {
+                putSerializable("article", it)
+            }
+            findNavController().navigate(
+                R.id.action_savedNewsFragment_to_articleFragment,
+                bundle
+            )
+        }
+    }
+
+    private fun setUpRecyclerView() {
+        newsAdapter = NewsAdapter()
+        rvSavedNews.apply {
+            adapter = newsAdapter
+            layoutManager = LinearLayoutManager(activity)
+        }
     }
 }
