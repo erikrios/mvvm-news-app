6928ca4b8f3b58eb0ee9849580e7d5f4aae447c3
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-06-02 10:38:10 +0700

    Add a method to check the internet connections

--- app/src/main/java/com/erikriosetiawan/newsapp/ui/NewsViewModel.kt
+++ app/src/main/java/com/erikriosetiawan/newsapp/ui/NewsViewModel.kt
@@ -1,9 +1,15 @@
 package com.erikriosetiawan.newsapp.ui
 
 import android.app.Application
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.ConnectivityManager.*
+import android.net.NetworkCapabilities.*
+import android.os.Build
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
+import com.erikriosetiawan.newsapp.NewsApplication
 import com.erikriosetiawan.newsapp.models.Article
 import com.erikriosetiawan.newsapp.models.NewsResponse
 import com.erikriosetiawan.newsapp.repository.NewsRepository
@@ -79,5 +85,33 @@ class NewsViewModel(app: Application, val newsRepository: NewsRepository) : Andr
 
     fun deleteArticle(article: Article) = viewModelScope.launch {
         newsRepository.deleteArticle(article)
+    }
+
+    private fun hasInternetConnection(): Boolean {
+        val connectivityManager =
+            getApplication<NewsApplication>().getSystemService(
+                Context.CONNECTIVITY_SERVICE
+            ) as ConnectivityManager
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            val activeNetwork = connectivityManager.activeNetwork ?: return false
+            val capabilities =
+                connectivityManager.getNetworkCapabilities(activeNetwork) ?: return false
+            return when {
+                capabilities.hasTransport(TRANSPORT_WIFI) -> true
+                capabilities.hasTransport(TRANSPORT_CELLULAR) -> true
+                capabilities.hasTransport(TRANSPORT_ETHERNET) -> true
+                else -> false
+            }
+        } else {
+            connectivityManager.activeNetworkInfo?.run {
+                return when (type) {
+                    TYPE_WIFI -> true
+                    TYPE_MOBILE -> true
+                    TYPE_ETHERNET -> true
+                    else -> false
+                }
+            }
+        }
+        return false
     }
 }
