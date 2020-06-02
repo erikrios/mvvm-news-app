b87c19576ee5bb6262ac14dc201023e6302fd213
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-06-02 10:56:48 +0700

    Add the application argument

--- app/src/main/java/com/erikriosetiawan/newsapp/ui/NewsViewModelProviderFactory.kt
+++ app/src/main/java/com/erikriosetiawan/newsapp/ui/NewsViewModelProviderFactory.kt
@@ -1,13 +1,18 @@
 package com.erikriosetiawan.newsapp.ui
 
+import android.app.Application
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import com.erikriosetiawan.newsapp.NewsApplication
 import com.erikriosetiawan.newsapp.repository.NewsRepository
 
 @Suppress("UNCHECKED_CAST")
-class NewsViewModelProviderFactory(val newsRepository: NewsRepository) : ViewModelProvider.Factory {
+class NewsViewModelProviderFactory(
+    val app: Application,
+    val newsRepository: NewsRepository
+) : ViewModelProvider.Factory {
 
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
-        return NewsViewModel(newsRepository) as T
+        return NewsViewModel(app, newsRepository) as T
     }
 }
