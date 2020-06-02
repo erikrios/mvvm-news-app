057d63f0a53ff9191669383e548cad389b4fbfd2
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-06-02 11:02:28 +0700

    Add toast to notify the user when they no have internet connection

--- app/src/main/java/com/erikriosetiawan/newsapp/ui/fragment/SearchNewsFragment.kt
+++ app/src/main/java/com/erikriosetiawan/newsapp/ui/fragment/SearchNewsFragment.kt
@@ -1,9 +1,9 @@
 package com.erikriosetiawan.newsapp.ui.fragment
 
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.widget.AbsListView
+import android.widget.Toast
 import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
@@ -73,7 +73,8 @@ class SearchNewsFragment : Fragment(R.layout.fragment_search_news) {
                 is Resource.Error -> {
                     hideProgressBar()
                     response.message?.let { message ->
-                        Log.e(TAG, "An error occured: $message")
+                        Toast.makeText(activity, "An error occured: $message", Toast.LENGTH_LONG)
+                            .show()
                     }
                 }
                 is Resource.Loading -> {
