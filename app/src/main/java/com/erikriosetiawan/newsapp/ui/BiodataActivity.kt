c3b8f1b80c84691b9fe49f45a29561ec2d9a3bc0
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-07-17 02:27:32 +0700

    Add action handler

--- app/src/main/java/com/erikriosetiawan/newsapp/ui/BiodataActivity.kt
+++ app/src/main/java/com/erikriosetiawan/newsapp/ui/BiodataActivity.kt
@@ -1,9 +1,13 @@
 package com.erikriosetiawan.newsapp.ui
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import com.erikriosetiawan.newsapp.R
+import com.erikriosetiawan.newsapp.util.SocialMedia
+import com.erikriosetiawan.newsapp.util.SocialMediaHelper
 
 class BiodataActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,6 +16,32 @@ class BiodataActivity : AppCompatActivity(), View.OnClickListener {
     }
 
     override fun onClick(v: View?) {
+        when (v?.id) {
+            SocialMediaHelper.getId(SocialMedia.GITHUB) -> openUrl(
+                SocialMediaHelper.getLink(
+                    SocialMedia.GITHUB
+                )
+            )
+            SocialMediaHelper.getId(SocialMedia.LINKED_IN) -> openUrl(
+                SocialMediaHelper.getLink(
+                    SocialMedia.LINKED_IN
+                )
+            )
+            SocialMediaHelper.getId(SocialMedia.FACEBOOK) -> openUrl(
+                SocialMediaHelper.getLink(
+                    SocialMedia.FACEBOOK
+                )
+            )
+            SocialMediaHelper.getId(SocialMedia.INSTAGRAM) -> openUrl(
+                SocialMediaHelper.getLink(
+                    SocialMedia.INSTAGRAM
+                )
+            )
+        }
+    }
 
+    private fun openUrl(url: String) {
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+        startActivity(intent)
     }
 }
