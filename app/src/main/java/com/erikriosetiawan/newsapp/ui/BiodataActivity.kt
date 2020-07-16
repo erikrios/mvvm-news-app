3872bf9bfb3ab4399045638624bd264f598aef0d
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-07-17 00:23:15 +0700

    Create BiodataActivity.kt

---
+++ app/src/main/java/com/erikriosetiawan/newsapp/ui/BiodataActivity.kt
@@ -0,0 +1,12 @@
+package com.erikriosetiawan.newsapp.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.erikriosetiawan.newsapp.R
+
+class BiodataActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_biodata)
+    }
+}
