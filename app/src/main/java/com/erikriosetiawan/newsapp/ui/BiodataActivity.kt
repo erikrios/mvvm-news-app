17d9b5ce60cef042f2b72019884164a7b1196751
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-07-17 02:11:27 +0700

    Implements OnClickListener interface

--- app/src/main/java/com/erikriosetiawan/newsapp/ui/BiodataActivity.kt
+++ app/src/main/java/com/erikriosetiawan/newsapp/ui/BiodataActivity.kt
@@ -2,11 +2,16 @@ package com.erikriosetiawan.newsapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.erikriosetiawan.newsapp.R
 
-class BiodataActivity : AppCompatActivity() {
+class BiodataActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_biodata)
+    }
+
+    override fun onClick(v: View?) {
+
     }
 }
